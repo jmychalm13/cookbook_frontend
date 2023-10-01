e89bb9f8c7310dea714654718dd1805432b3f37e
“Jessica <jmychalm13@gmail.com>
2023-10-01 15:56:53 -0500

    added gutters

--- src/Content.jsx
+++ src/Content.jsx
@@ -17,28 +17,33 @@ export function Content() {
     <div>
       <h1 className="text-center">All Recipes</h1>
       <div className="container">
-        <div className="recipes-container row">
+        <div className="recipes-container row row-cols-1 row-cols-md-3 g-2">
           {recipes.map((recipe, index) => (
-            <div key={index} className="card col-4 gx-2 gy-2">
-              <h3 className="fw-bold">{recipe.name}</h3>
-              <div className="card-body">
-                <p>
-                  <strong>Chef:</strong> {recipe.chef}
-                </p>
-                <p>
-                  <strong>Indgredients:</strong>
-                </p>
-                {recipe.ingredients_list.length > 0 ? (
-                  recipe.ingredients_list.map((ingredient, index2) => {
-                    return <p key={index2}>{ingredient}</p>;
-                  })
-                ) : (
-                  <p>No ingredients listed.</p>
-                )}
-                {/* {recipe.ingredients_list.map((ingredient, index2) => (
-                  <p key={index2}>{ingredient}</p>
-                ))} */}
-                {/* todo: total cooktime */}
+            <div key={index} className="col">
+              <div className="card">
+                <h3 className="text-center fw-bold">{recipe.name}</h3>
+                <div className="card-body">
+                  <p>
+                    <strong>Chef:</strong> {recipe.chef}
+                  </p>
+                  <p>
+                    <strong>Ingredients:</strong>
+                  </p>
+                  <div className="ingredients-container">
+                    {recipe.ingredients_list.length > 0 ? (
+                      recipe.ingredients_list.map((ingredient, index2) => {
+                        return <p key={index2}>{ingredient}</p>;
+                      })
+                    ) : (
+                      <p>No ingredients listed.</p>
+                    )}
+                  </div>
+                  <div className="time-container">
+                    <p>
+                      <strong>Total cook time:</strong> {recipe.total_time}
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           ))}
