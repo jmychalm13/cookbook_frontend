ee2eb3c70d89512f22ecc7af0e2bd005d0dc0dc7
“Jessica <jmychalm13@gmail.com>
2023-10-09 06:29:44 -0500

    working signup component, not complete, functional

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,4 +1,30 @@
+import axios from "axios";
+import { useState } from "react";
+
 export function Signup() {
+  const [errors, setErrors] = useState([]);
+  const [name, setName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors([]);
+    const params = new FormData(event.target);
+    console.log("I ran");
+    console.log(params);
+    axios
+      .post("http://localhost:3000/users.json", params)
+      .then((response) => {
+        console.log(response.data);
+        event.target.reset();
+        // where you want to redirect to
+        // window.location.href = "/";
+      })
+      .catch((error) => {
+        console.log(error.response.data.errors);
+        setErrors(error.response.data.errors);
+      });
+  };
+
   return (
     <div>
       <section className="vh-100">
@@ -9,14 +35,21 @@ export function Signup() {
                 <div className="card-body p-md-5">
                   <div className="row justify-content-center">
                     <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                      <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+                      <h1 className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</h1>
 
-                      <form action="" className="mx-1 mx-md-4">
+                      <form onSubmit={handleSubmit} className="mx-1 mx-md-4">
                         <div className="d-flex flex-row align-items-center mb-4">
                           <i className="fas fa-user fa-lg me-3 fa-fw"></i>
                           <div className="form-floating flex-fill mb-0">
-                            <input type="text" id="username" className="form-control" />
-                            <label htmlFor="username" className="form-label">
+                            <input
+                              value={name}
+                              onChange={(event) => setName(event.target.value)}
+                              type="text"
+                              id="name"
+                              className="form-control"
+                              name="name"
+                            />
+                            <label htmlFor="name" className="form-label">
                               Your name
                             </label>
                           </div>
@@ -25,7 +58,7 @@ export function Signup() {
                         <div className="d-flex flex-row align-items-center mb-4">
                           <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
                           <div className="form-floating flex-fill mb-0">
-                            <input type="email" id="email" className="form-control" />
+                            <input type="email" id="email" className="form-control" name="email" />
                             <label htmlFor="email" className="form-label">
                               Email
                             </label>
@@ -35,8 +68,8 @@ export function Signup() {
                         <div className="d-flex flex-row align-items-center mb-4">
                           <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
                           <div className="form-floating flex-fill mb-0">
-                            <input type="password" id="form3Example4c" className="form-control" />
-                            <label className="form-label" htmlFor="form3Example4c">
+                            <input type="password" id="password" className="form-control" name="password" />
+                            <label className="form-label" htmlFor="password">
                               Password
                             </label>
                           </div>
@@ -45,13 +78,31 @@ export function Signup() {
                         <div className="d-flex flex-row align-items-center mb-4">
                           <i className="fas fa-key fa-lg me-3 fa-fw"></i>
                           <div className="form-floating flex-fill mb-0">
-                            <input type="password" id="form3Example4cd" className="form-control" />
-                            <label className="form-label" htmlFor="form3Example4cd">
+                            <input
+                              type="password"
+                              id="password_confirmation"
+                              className="form-control"
+                              name="password-confirmation"
+                            />
+                            <label className="form-label" htmlFor="password_confirmation">
                               Repeat your password
                             </label>
                           </div>
                         </div>
+
+                        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                          <button type="submit" className="btn btn-primary btn-lg">
+                            Signup
+                          </button>
+                        </div>
                       </form>
+                    </div>
+                    <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                      <img
+                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                        alt="sample-image"
+                        className="img-fluid"
+                      />
                     </div>
                   </div>
                 </div>
